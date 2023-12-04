8bb8e220493a3d16aee3ca4c4c0509c7fc3c0593
BayuKartawan <kartawanbayu@gmail.com>
2023-12-04 21:57:14 +0800

    up des

--- FrontEnd/src/page/Deskripsi.jsx
+++ FrontEnd/src/page/Deskripsi.jsx
@@ -11,9 +11,12 @@ const Deskripsi = () => {
     return (
         <div>
             <Navbar linkKelas={customLinkStyle} />
-            <section className="komponendes">
+            <section className="komponendes" style={{ marginTop: "20px" }}>
                 <div className="w-layout-blockcontainer deskontainer w-container">
-                    <Deskripsis />
+                    <Deskripsis
+                        Judul="&quot;Californication&quot; - Red Hot Chili Peppers (Cover by First to Eleven)"
+                        Vidio="f1iW734wGns"
+                        Deskripsi="gLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
                 </div>
             </section>
             <Footer />
