b5303e38e3a32829123f16769c21c6921ef85d7d
Kevinrahmat013 <146647151+Kevinrahmat013@users.noreply.github.com>
2023-12-14 12:13:56 +0700

    testing

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,7 +4,7 @@ import { register, login, logout } from "../controllers/auth.js";
 const router = express.Router();
 router.get("/tes", function(req,res,next) {
   res.send('berhasil tes');
-  );
+});
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
