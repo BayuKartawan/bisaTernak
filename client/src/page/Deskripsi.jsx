7ec9d6c9c71af1bf26773e055594faca78211a5d
BayuKartawan <kartawanbayu@gmail.com>
2023-12-16 20:02:36 +0800

    up

--- client/src/page/Deskripsi.jsx
+++ client/src/page/Deskripsi.jsx
@@ -3,26 +3,24 @@ import Deskripsis from "../component/CardDeskripsi"
 const Deskripsi = () => {
 
     return (
-        <div>
-            <section className="komponendes" style={{ marginTop: "20px" }}>
-                <div className="w-layout-blockcontainer deskontainer w-container">
-                    <Deskripsis
-                        Judul="Ternak Sapi Gembira"
-                        Vidio="yidbHYrDjJM"
-                        Deskripsi={
-                            <>Peternak sapi adalah individu atau kelompok yang berdedikasi untuk membimbing, merawat, dan mengelola sapi dengan penuh perhatian. Mereka memiliki pengetahuan yang mendalam tentang kebutuhan dan perilaku sapi, serta keterampilan dalam merawat dan memelihara ternak dengan baik.
-                                <br /><br />
-                                Dalam setiap langkah dari pemilihan bibit, perawatan harian, hingga manajemen pembiakan, peternak sapi berusaha menciptakan lingkungan yang sehat dan produktif bagi ternaknya. Mereka memahami pentingnya nutrisi yang tepat, perawatan medis yang baik, serta pemeliharaan fasilitas yang bersih dan aman.
-                                <br /><br />
-                                Peternak sapi tidak hanya memiliki hubungan kerja dengan ternaknya, tetapi juga mengembangkan ikatan khusus dengan setiap individu sapi. Mereka melibatkan diri secara aktif dalam memantau kesehatan dan kesejahteraan sapi, serta memberikan perhatian khusus kepada setiap fase kehidupan ternak mereka, dari kelahiran hingga pertumbuhan.
-                                <br /><br />
-                                Selain itu, peternak sapi sering kali berperan sebagai pelopor dalam praktik pertanian yang berkelanjutan dan etis. Mereka bekerja keras untuk memahami dan menerapkan praktik-praktik terbaik dalam manajemen limbah, penggunaan lahan yang bijaksana, dan perlindungan lingkungan.
-                                <br /><br />
-                                Dengan komitmen dan dedikasi mereka, peternak sapi tidak hanya menciptakan kehidupan yang baik bagi ternaknya, tetapi juga berkontribusi pada pasokan pangan yang aman dan berkualitas tinggi. Profesi ini mencerminkan harmoni antara keahlian praktis, cinta terhadap hewan, dan tanggung jawab terhadap lingkungan, menjadikannya bagian integral dari sektor pertanian yang berkelanjutan.
-                            </>} />
-                </div>
-            </section>
-        </div>
+        <section className="komponendes" style={{ marginTop: "20px" }}>
+            <div className="w-layout-blockcontainer deskontainer w-container">
+                <Deskripsis
+                    Judul="Ternak Sapi Gembira"
+                    Vidio="yidbHYrDjJM"
+                    Deskripsi={
+                        <>Peternak sapi adalah individu atau kelompok yang berdedikasi untuk membimbing, merawat, dan mengelola sapi dengan penuh perhatian. Mereka memiliki pengetahuan yang mendalam tentang kebutuhan dan perilaku sapi, serta keterampilan dalam merawat dan memelihara ternak dengan baik.
+                            <br /><br />
+                            Dalam setiap langkah dari pemilihan bibit, perawatan harian, hingga manajemen pembiakan, peternak sapi berusaha menciptakan lingkungan yang sehat dan produktif bagi ternaknya. Mereka memahami pentingnya nutrisi yang tepat, perawatan medis yang baik, serta pemeliharaan fasilitas yang bersih dan aman.
+                            <br /><br />
+                            Peternak sapi tidak hanya memiliki hubungan kerja dengan ternaknya, tetapi juga mengembangkan ikatan khusus dengan setiap individu sapi. Mereka melibatkan diri secara aktif dalam memantau kesehatan dan kesejahteraan sapi, serta memberikan perhatian khusus kepada setiap fase kehidupan ternak mereka, dari kelahiran hingga pertumbuhan.
+                            <br /><br />
+                            Selain itu, peternak sapi sering kali berperan sebagai pelopor dalam praktik pertanian yang berkelanjutan dan etis. Mereka bekerja keras untuk memahami dan menerapkan praktik-praktik terbaik dalam manajemen limbah, penggunaan lahan yang bijaksana, dan perlindungan lingkungan.
+                            <br /><br />
+                            Dengan komitmen dan dedikasi mereka, peternak sapi tidak hanya menciptakan kehidupan yang baik bagi ternaknya, tetapi juga berkontribusi pada pasokan pangan yang aman dan berkualitas tinggi. Profesi ini mencerminkan harmoni antara keahlian praktis, cinta terhadap hewan, dan tanggung jawab terhadap lingkungan, menjadikannya bagian integral dari sektor pertanian yang berkelanjutan.
+                        </>} />
+            </div>
+        </section>
     );
 };
 
--- client/src/page/Up.jsx
+++ client/src/page/Up.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import CardKelas from "../component/CardKelas";
 import UpImg from "../assets/Symbol/AddImage.png"
 import Unggah from "../assets/Symbol/Up.png"
+import "../media/up.css"
 
 const Up = () => {
 
@@ -24,111 +25,147 @@ const Up = () => {
         pointerEvents: "none"
     };
 
+    //form set
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log("Form submitted!");
+
+        // Tambahkan logika pengiriman data atau tindakan yang sesuai di sini
+        try {
+            // ... Logika pengiriman data ...
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    //pop up
+    const [isPopupVisible, setPopupVisible] = useState(false);
+
+    const showPopup = () => {
+        setPopupVisible(true);
+    };
+
+    const hidePopup = () => {
+        setPopupVisible(false);
+    };
+
     return (
-        <div>
-            <section className="componenupload" style={{ marginTop: "10px" }}>
-                <div className="w-layout-blockcontainer container-2 w-container">
-                    <h3 className="heading-4">Unggah koten</h3>
-                    <p className="paragraph-4" style={{ padding: "20px", borderRadius: "10px" }}>Unggah konten dalam ruang linkup hewan ruminansia</p>
-                    <div className="form-block">
 
-                        <form name="wf-form-unggah"
-                            method="post" >
+        <section className="componenupload" style={{ marginTop: "10px" }}>
+            <div className="w-layout-blockcontainer container-2 w-container">
+                <h3 className="heading-4">Unggah koten</h3>
+                <p className="paragraph-4" style={{ padding: "20px", borderRadius: "10px" }}>Unggah konten dalam ruang linkup hewan ruminansia</p>
+                <div className="form-block">
 
-                            <label className="deslabel"
-                                htmlFor="file"
-                                style={{
-                                    borderRadius: "5px",
-                                    textAlign: "center",
-                                    marginBottom: "20px",
-                                    paddingTop: "10px",
-                                    paddingBottom: "10px",
-                                    backgroundColor: "#FAF8ED",
-                                    borderStyle: "dashed", // Gaya garis jalan (dashed)
-                                    borderColor: "#F86F03", // Warna garis jalan
-                                }}>
-                                <img
-                                    src={UpImg}
-                                    height="30px"
-                                    loading="lazy"
-                                    style={{ marginBottom: "5px", marginRight: "5px" }}>
-                                </img><p>Unggah sampul kelas*</p>
-                                {selectedImage && (
-                                    <div style={{ backgroundColor: "white", paddingTop: "10px", display: "flex", flexDirection: "column", alignItems: "center", borderTop: "1px solid lightgray", marginTop: "10px" }} >
-                                        <p style={{ fontFamily: "League Spartan" }}>Pratinjau :</p>
-                                        <CardKelas
-                                            Link={customCardKelasStyle}
-                                            img={selectedImage}
-                                            judul="(pratinjau)"
-                                            oleh="(pratinjau)" />
-                                    </div>
-                                )}
-                            </label>
+                    <form name="wf-form-unggah"
+                        method="post"
+                        onSubmit={handleSubmit} >
 
-                            {/* upload cover */}
-                            <input
-                                style={{ display: "none" }}
-                                type="file"
-                                id="file"
-                                name=""
-                                onChange={handleImageChange}
-                            />
-
-                            {/* link youtube */}
-                            <label htmlFor="linkVidio" className="deslabel">ID vidio youtube (privasi)*</label>
-                            <input
-                                type="text"
-                                className="desfield w-input"
-                                maxLength="256"
-                                name="linkVidio"
-                                data-name="linkVidio"
-                                placeholder="contoh : https://www.youtube.com/bausaUBbjbU6HB..."
-                                required="" />
-
-                            <label htmlFor="judul" className="deslabel">Judul*</label>
-                            <input
-                                type="text"
-                                className="desfield w-input"
-                                maxLength="256"
-                                name="judul"
-                                data-name="judul"
-                                placeholder="Masukkan judul materi..."
-                                required="" />
-
-                            {/* Form Deskripsi */}
-                            <p className="deslabel">Deskripsi*</p>
-                            <div style={{
-                                height: "300px",
-                                overflow: "scroll",
-                                border: "1px solid lightgray",
-                                marginBottom: "10px",
+                        <label className="deslabel"
+                            htmlFor="file"
+                            style={{
+                                borderRadius: "5px",
+                                textAlign: "center",
+                                marginBottom: "20px",
+                                paddingTop: "10px",
+                                paddingBottom: "10px",
+                                backgroundColor: "#FAF8ED",
+                                borderStyle: "dashed", // Gaya garis jalan (dashed)
+                                borderColor: "#F86F03", // Warna garis jalan
                             }}>
-                                <ReactQuill
-                                    style={{
-                                        height: "100%",
-                                        border: "none",
-                                    }}
-                                    theme="snow"
-                                    value=""
-                                    onChange=""
-                                />
-                            </div>
+                            <img
+                                src={UpImg}
+                                height="30px"
+                                loading="lazy"
+                                style={{ marginBottom: "5px", marginRight: "5px" }}>
+                            </img><p>Unggah sampul kelas*</p>
+                            {selectedImage && (
+                                <div style={{ backgroundColor: "white", paddingTop: "10px", display: "flex", flexDirection: "column", alignItems: "center", borderTop: "1px solid lightgray", marginTop: "10px" }} >
+                                    <p style={{ fontFamily: "League Spartan" }}>Pratinjau :</p>
+                                    <CardKelas
+                                        Link={customCardKelasStyle}
+                                        img={selectedImage}
+                                        judul="(pratinjau)"
+                                        oleh="(pratinjau)" />
+                                </div>
+                            )}
+                        </label>
 
-                            <button
-                                style={{ marginBottom: "20px", marginTop: "20px", width: "100%" }}
-                                type="submit"
-                                data-wait="Please wait..."
-                                className="buttontambah w-button"
-                            >
-                                <img src={Unggah} height="20px" loading="lazy" alt="Unggah" style={{ marginBottom: "5px", marginRight: "5px" }} />
-                                Unggah
-                            </button>
-                        </form>
+                        {/* upload cover */}
+                        <input
+                            style={{ display: "none" }}
+                            type="file"
+                            id="file"
+                            name=""
+                            onChange={handleImageChange}
+                        />
 
-                    </div>
+                        {/* link youtube */}
+                        <label htmlFor="linkVidio" className="deslabel">
+                            ID vidio youtube (privasi)*<button onClick={showPopup}>?</button>
+
+                            {isPopupVisible && (
+                                <div className="popup">
+                                    <p>Cara melihat id Youtube <br />
+                                        - Contoh url youtube : https://www.youtube.com/watch?v=sSuyO0f9KmM <br />
+                                        - ID dilihat setelah v= yaitu : <b>sSuyO0f9KmM</b> <br />
+                                        - Jumlah id youtub 11 angka/huruf</p>
+                                    <button onClick={hidePopup} className="popup-click">Close</button>
+                                </div>
+                            )}
+                        </label>
+                        <input
+                            type="text"
+                            className="desfield w-input"
+                            maxLength="256"
+                            name="linkVidio"
+                            data-name="linkVidio"
+                            placeholder="contoh : sSuyO0f9KmM"
+                            required="" />
+
+                        {/* INPUT JUDUL} */}
+                        <label htmlFor="judul" className="deslabel">Judul*</label>
+                        <input
+                            type="text"
+                            className="desfield w-input"
+                            maxLength="256"
+                            name="judul"
+                            data-name="judul"
+                            placeholder="Masukkan judul materi..."
+                            required="" />
+
+                        {/* Form Deskripsi */}
+                        <p className="deslabel">Deskripsi*</p>
+                        <div style={{
+                            height: "300px",
+                            overflow: "scroll",
+                            border: "1px solid lightgray",
+                            marginBottom: "10px",
+                        }}>
+                            <ReactQuill
+                                style={{
+                                    height: "100%",
+                                    border: "none",
+                                }}
+                                theme="snow"
+                                value=""
+                                onChange=""
+                            />
+                        </div>
+
+                        <button
+                            style={{ marginBottom: "20px", marginTop: "20px", width: "100%" }}
+                            type="submit"
+                            data-wait="Please wait..."
+                            className="buttontambah w-button"
+                        ><img src={Unggah} height="20px" loading="lazy" alt="Unggah" style={{ marginBottom: "5px", marginRight: "5px" }} />
+                            Unggah
+                        </button>
+                    </form>
+
                 </div>
-            </section>
-        </div>
+            </div>
+        </section >
     )
 }
 
